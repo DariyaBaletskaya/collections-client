2381d9474a37d92282be118311264fbc488c6461
kirill <kirillterentyev1999@gmail.com>
2019-03-04 12:08:30 +0200

    Added item adding/editing screen

---
+++ app/src/main/java/onpu/pnit/collectionsclient/activity/ItemAddEditActivity.java
@@ -0,0 +1,15 @@
+package onpu.pnit.collectionsclient.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import onpu.pnit.collectionsclient.R;
+
+import android.os.Bundle;
+
+public class ItemAddEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_add_edit);
+    }
+}
