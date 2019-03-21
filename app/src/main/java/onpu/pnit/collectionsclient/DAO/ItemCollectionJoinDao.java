d49189aa69c05762f4ed01b6b8f18298acb9e71d
kirill <kirillterentyev1999@gmail.com>
2019-03-21 19:03:45 +0200

    Fixed ItemCollectionDao Queries

--- app/src/main/java/onpu/pnit/collectionsclient/DAO/ItemCollectionJoinDao.java
+++ app/src/main/java/onpu/pnit/collectionsclient/DAO/ItemCollectionJoinDao.java
@@ -11,11 +11,13 @@ import onpu.pnit.collectionsclient.entities.Item;
 
 @Dao
 public interface ItemCollectionJoinDao {
-    @Query("SELECT * FROM collections c INNER JOIN item_collection_join ic ON c.collection_id=ic.collection_id " +
-            "WHERE ic.item_id=:itemId")
+    @Query("SELECT * FROM collections c WHERE c.collection_id IN " +
+            "(SELECT collection_id FROM item_collection_join ic " +
+            "WHERE ic.item_id=:itemId AND ic.collection_id=c.collection_id)")
     LiveData<List<Collection>> getCollectionsForItem(int itemId);
 
-    @Query("SELECT * FROM items i INNER JOIN item_collection_join ic ON i.item_id=ic.item_id " +
-            "WHERE ic.collection_id=:collectionId")
+    @Query("SELECT * FROM items i WHERE i.item_id IN " +
+            "(SELECT item_id FROM item_collection_join ic " +
+            "WHERE ic.collection_id=:collectionId AND ic.item_id=i.item_id)")
     LiveData<List<Item>> getItemsForCollection(int collectionId);
 }
