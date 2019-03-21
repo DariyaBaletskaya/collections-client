c1d5368dab3be246cfd47b5f548c7147182845e2
kirill <kirillterentyev1999@gmail.com>
2019-03-21 20:23:38 +0200

    Commit ItemDao

--- app/src/main/java/onpu/pnit/collectionsclient/DAO/ItemDao.java
+++ app/src/main/java/onpu/pnit/collectionsclient/DAO/ItemDao.java
@@ -10,27 +10,34 @@ import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
-import onpu.pnit.collectionsclient.entities.Items;
-
+import onpu.pnit.collectionsclient.entities.Item;
 
 @Dao
-public interface ItemDAO {
+public interface ItemDao {
     @Insert
-    void insertItem(Items items);
+    void insertItem(Item item);
 
     @Delete
-    void deleteItem(Items items);
+    void deleteItem(Item item);
 
     @Update
-    void updateItem(Items... items);
+    void updateItem(Item item);
 
-    @Query("DELETE FROM items WHERE userId=:userId")
+    @Update
+    void updateItems(Item... items);
+
+    @Query("DELETE FROM Items WHERE user_id=:userId")
     void deleteAllItemsForUser(int userId);
 
-    @Query("SELECT * from items WHERE userId=:userId")
-    LiveData<List<Items>> getItemsForUser(int userId);
+    @Query("SELECT * from Items WHERE user_id=:userId")
+    LiveData<List<Item>> getItemsForUser(int userId);
 
-    @Query("SELECT * from items WHERE title=:title")
-    LiveData<List<Items>> getItemsByTitle(String title);
+    @Query("SELECT * from Items WHERE item_title=:title")
+    LiveData<List<Item>> getItemsByTitle(String title);
 
+    @Query("SELECT * from Items")
+    LiveData<List<Item>> getAllItems();
+
+    @Insert
+    void insertAllItems(Item... items);
 }
