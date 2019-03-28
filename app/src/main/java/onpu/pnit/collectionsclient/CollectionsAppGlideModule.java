c3c5b06328d26b721b2d5007bcb6997e5d5034e2
kirill <kirillterentyev1999@gmail.com>
2019-03-28 21:25:10 +0200

    Added Glide dependencies and AppGlideModule class Updated gradle to 3.3.2

---
+++ app/src/main/java/onpu/pnit/collectionsclient/CollectionsAppGlideModule.java
@@ -0,0 +1,8 @@
+package onpu.pnit.collectionsclient;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class CollectionsAppGlideModule extends AppGlideModule {
+}
