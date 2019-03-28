520cdc29dd55a5b1942fa0306b8486ee79cff7bb
kirill <kirillterentyev1999@gmail.com>
2019-03-28 23:42:38 +0200

    Added OkHttp integration to AppGlideModule

--- app/src/main/java/onpu/pnit/collectionsclient/CollectionsAppGlideModule.java
+++ app/src/main/java/onpu/pnit/collectionsclient/CollectionsAppGlideModule.java
@@ -1,8 +1,28 @@
 package onpu.pnit.collectionsclient;
 
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.Excludes;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpLibraryGlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
 
+import java.io.InputStream;
+
+import androidx.annotation.NonNull;
+import okhttp3.OkHttpClient;
+
 @GlideModule
+@Excludes(OkHttpLibraryGlideModule.class)
 public final class CollectionsAppGlideModule extends AppGlideModule {
+    @Override
+    public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
+        super.registerComponents(context, glide, registry);
+        OkHttpClient okHttpClient = new OkHttpClient();
+        registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(okHttpClient));
+    }
 }
